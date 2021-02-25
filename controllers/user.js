6ccb572109683843b1b907c219bd4b32442998cc
RitaZ <xiaoyz109@gmail.com>
2021-02-25 14:11:53 -0800

    remove period

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const db = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-require(".dotenv").config();
+require("dotenv").config();
 
 const authenticateMe = (req) => {
   let token = false;
