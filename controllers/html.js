b9711d4b4da47a6643e4500313cdcc0ca859feaf
chomieu <chomieu@gmail.com>
2021-02-26 12:50:15 -0800

    Removed / route from html.js

--- controllers/html.js
+++ controllers/html.js
@@ -1,18 +1,6 @@
 const router = require("express").Router();
 const db = require("../models");
 
-// Populates homepage with user data
-router.get("/", (req, res) => {
-  db.User.findOne(req.body)
-    .populate("records")
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
 // Finds all songs in the database
 // Returns 1.song id and 2.formatted-name 
 router.get("/api/song", (req, res) => {
