c7f46c318b2fc91b31741239aef25ac992d224e2
Samuel Fox <samueljasonfox@gmail.com>
2021-02-27 19:54:57 -0800

    controllers/mp3.js - solved bug - in an event of a song name having a '/' in the name the lrc file cannot be created.

--- controllers/mp3.js
+++ controllers/mp3.js
@@ -16,12 +16,27 @@ router.post("/api/download", (req, res) => {
       } else {
         musicApi.search(req.body.name, "song").then((songResult) => {
 
+          console.log(19, songResult.content[0])
+
           const songName = songResult.content[0].name.toLowerCase();
           const artistName = songResult.content[0].artist.name.toLowerCase();
 
           fs.readdir(path.join(__dirname, "../lrc"), (err, data) => {
+
             if (data.indexOf(`${songName} - ${artistName}.lrc`) === -1) {
-              createLrc(songName, artistName);
+              console.log(27, songName)
+              console.log(28, artistName)
+
+              // bug: user search "let it go original"
+              // musicApi response for song name  - 'Let It Go (From "Frozen"/Soundtrack Version)'
+              // the '/' causes an issue for createLrc() filepath
+              // split on the unwanted characters and join with single space to remove unwanted characters
+              let test = songName
+              test = test.split('/').join(' ')
+              console.log(36, test)
+
+
+              createLrc(test, artistName);
 
               const options = {
                 method: "GET",
@@ -37,25 +52,38 @@ router.post("/api/download", (req, res) => {
               axios
                 .request(options)
                 .then(function (response) {
-                  console.log(data)
+                  console.log(55, response.data)// see below
+                  // 55 {
+                  //   Status: 'Fail',
+                  //   Status_Code: 103,
+                  //   Warining: 'Video Id Maybe Invalid Or Retry Again May Work'
+                  // }
                   const mp3Url = response.data.Download_url;
-                  db.Song.create({
-                    name: songName,
-                    artist: artistName,
-                    lyrics: `${songName} - ${artistName}.lrc`,
-                    mixed: mp3Url,
-                  }).then(() => {
-                    res.send("downloaded");
-                  })
-                    // added-sjf
-                    .catch(err => { res.status(500).send(err) })
+                  // added-sjf
+                  if (mp3Url) {
+                    db.Song.create({
+                      name: songName,
+                      artist: artistName,
+                      lyrics: `${songName} - ${artistName}.lrc`,
+
+                      mixed: mp3Url,
+                    }).then(() => {
+                      res.send("downloaded");
+                    })
+                      // added-sjf
+                      .catch(err => {
+                        console.log(67)
+                        res.status(500).send(err)
+                      })
+                  } else {
+                    res.status(404).send(response.data) // see example on line 55
+                  }
                 })
                 .catch(function (error) {
                   console.error(error)
                   // added-sjf
                   res.status(500).send(err)
                 });
-
             } else {
               res.send("this song already existed!");
             }
@@ -64,7 +92,10 @@ router.post("/api/download", (req, res) => {
       }
     })
     // added-sjf
-    .catch(err => { res.status(500).send(err) })
+    .catch(err => {
+      console.log(84)
+      res.status(500).send(err)
+    })
 });
 
 module.exports = router;
