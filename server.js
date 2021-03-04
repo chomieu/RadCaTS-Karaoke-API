81d9ec0477a2e10e3953c9f904d99fb2cd70544e
chomieu <chomieu@gmail.com>
2021-03-04 10:48:48 -0800

    Readded cors

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const cors = require("cors");
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-  // origin: ["https://radcats-karaoke.herokuapp.com/"],
-}));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+app.use(cors());
 
 // Database
 const mongoose = require("mongoose");
